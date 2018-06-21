3203749701f52522e024bec27408e1920c4582a5
James Lim <email@wanseob.com>
2018-06-21 16:45:05 +0900

    Draft version

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+var commitground = require('commitground');
+
+describe('Suite of test', function() {
+  it('should be passed', function() {
+    assert.ok(true)
+  })
+})
